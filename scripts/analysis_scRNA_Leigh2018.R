b8d1923e17f974bd61d6c3cb1ef7e9d160a926be
jingkui.wang <jingkui.wang@imp.ac.at>
2026-04-14 13:47:29 +0200

    save the analysis

--- scripts/analysis_scRNA_Leigh2018.R
+++ scripts/analysis_scRNA_Leigh2018.R
@@ -95,6 +95,138 @@ saveRDS(seu_14dpa, file = paste0(RdataDir, '/seuratObj_processedWhited_14dpa.rds
 saveRDS(seu_23dpa, file = paste0(RdataDir, '/seuratObj_processedWhited_23dpa.rds'))
 
 
+##########################################
+# change the gene names 
+##########################################
+#seu_dpa0 = readRDS(file = paste0(RdataDir, '/seuratObj_processedWhited_0dpa.rds'))
+#aa = seu_dpa0
+#counts = aa@assays$RNA@counts
+#metadata = aa@meta.data
+#seu_dpa3 = readRDS(file = paste0(RdataDir, '/seuratObj_processedWhited_3dpa.rds'))
+#seu_dpa14 = readRDS(file = paste0(RdataDir, '/seuratObj_processedWhited_14dpa.rds'))
+#seu_dpa23 = readRDS(file = paste0(RdataDir, '/seuratObj_processedWhited_23dpa.rds'))
 
+########################################################
+########################################################
+# Section II: check marker genes 
+# 
+########################################################
+########################################################
+seu_dpa0 = readRDS(file = paste0(RdataDir, '/seuratObj_processedWhited_0dpa.rds'))
+#aa = seu_dpa0
+#counts = aa@assays$RNA@counts
+#metadata = aa@meta.data
+seu_dpa3 = readRDS(file = paste0(RdataDir, '/seuratObj_processedWhited_3dpa.rds'))
+seu_dpa14 = readRDS(file = paste0(RdataDir, '/seuratObj_processedWhited_14dpa.rds'))
+seu_dpa23 = readRDS(file = paste0(RdataDir, '/seuratObj_processedWhited_23dpa.rds'))
+
+
+p1 = DimPlot(seu_dpa0, group.by = 'celltypes', label = TRUE, repel = TRUE) + ggtitle('dpa0') +
+  NoLegend()
+p2 = DimPlot(seu_dpa3, group.by = 'celltypes', label = TRUE, repel = TRUE) + ggtitle('dpa3') +
+  NoLegend()
+p3 = DimPlot(seu_dpa14, group.by = 'celltypes', label = TRUE, repel = TRUE) + ggtitle('dpa14') +
+  NoLegend()
+p4 = DimPlot(seu_dpa0, group.by = 'celltypes', label = TRUE, repel = TRUE) + ggtitle('dpa23') +
+  NoLegend()
+
+(p1 + p2)/(p3 + p4) 
+
+ggsave(filename = paste0(resDir, '/tSNE_Whited_celltypes.pdf'), 
+       width = 16, height = 12)
+
+### dpa0
+features = c(rownames(seu_dpa0)[grep('WNT3A', rownames(seu_dpa0))],
+             rownames(seu_dpa0)[grep('WNT5A', rownames(seu_dpa0))],
+             rownames(seu_dpa0)[grep('TGFB', rownames(seu_dpa0))],
+             rownames(seu_dpa0)[grep('INHA', rownames(seu_dpa0))])
+FeaturePlot(seu_dpa0, 
+            features = features,
+            ncol = 2)
+
+ggsave(filename = paste0(resDir, '/Whiteddataset_tSNE_dpa0_featureplot.pdf'), 
+       width = 12, height = 18)
+
+VlnPlot(seu_dpa0, features = features, group.by = 'celltypes', ncol = 1)
+
+ggsave(filename = paste0(resDir, '/Whiteddataset_tSNE_dpa0_VlnPlot.pdf'), 
+       width = 8, height = 28)
+
+write.csv2(features, file = paste0(resDir, '/Whiteddataset_dpa0_featuresList.csv'), 
+           row.names = FALSE)
+
+
+### dpa3
+aa = seu_dpa3
+features = c(rownames(aa)[grep('WNT3A', rownames(aa))],
+             rownames(aa)[grep('WNT5A', rownames(aa))],
+             rownames(aa)[grep('WNT1_', rownames(aa))],
+             rownames(aa)[grep('WNT2_', rownames(aa))],
+             rownames(aa)[grep('WNT8_', rownames(aa))],
+             rownames(aa)[grep('TGFB', rownames(aa))],
+             rownames(aa)[grep('INHA', rownames(aa))])
+FeaturePlot(aa, 
+            features = features,
+            ncol = 2)
+
+ggsave(filename = paste0(resDir, '/Whiteddataset_tSNE_dpa3_featureplot.pdf'), 
+       width = 12, height = 18)
+
+VlnPlot(aa, features = features, group.by = 'celltypes', ncol = 1)
+
+ggsave(filename = paste0(resDir, '/Whiteddataset_tSNE_dpa3_VlnPlot.pdf'), 
+       width = 8, height = 28)
+
+write.csv2(features, file = paste0(resDir, '/Whiteddataset_dpa3_featuresList.csv'), 
+           row.names = FALSE)
+
+### dpa14
+aa = seu_dpa14
+features = c(rownames(aa)[grep('WNT3A', rownames(aa))],
+             rownames(aa)[grep('WNT5A', rownames(aa))],
+             rownames(aa)[grep('WNT1_', rownames(aa))],
+             rownames(aa)[grep('WNT2_', rownames(aa))],
+             rownames(aa)[grep('WNT8_', rownames(aa))],
+             rownames(aa)[grep('TGFB', rownames(aa))],
+             rownames(aa)[grep('INHA', rownames(aa))])
+FeaturePlot(aa, 
+            features = features,
+            ncol = 2)
+
+ggsave(filename = paste0(resDir, '/Whiteddataset_tSNE_dpa14_featureplot.pdf'), 
+       width = 12, height = 18)
+
+VlnPlot(aa, features = features, group.by = 'celltypes', ncol = 1)
+
+ggsave(filename = paste0(resDir, '/Whiteddataset_tSNE_dpa14_VlnPlot.pdf'), 
+       width = 8, height = 28)
+
+write.csv2(features, file = paste0(resDir, '/Whiteddataset_dpa14_featuresList.csv'), 
+           row.names = FALSE)
+
+
+### dpa14
+aa = seu_dpa23
+features = c(rownames(aa)[grep('WNT3A', rownames(aa))],
+             rownames(aa)[grep('WNT5A', rownames(aa))],
+             rownames(aa)[grep('WNT1_', rownames(aa))],
+             rownames(aa)[grep('WNT2_', rownames(aa))],
+             rownames(aa)[grep('WNT8_', rownames(aa))],
+             rownames(aa)[grep('TGFB', rownames(aa))],
+             rownames(aa)[grep('INHA', rownames(aa))])
+FeaturePlot(aa, 
+            features = features,
+            ncol = 2)
+
+ggsave(filename = paste0(resDir, '/Whiteddataset_tSNE_dpa23_featureplot.pdf'), 
+       width = 12, height = 18)
+
+VlnPlot(aa, features = features, group.by = 'celltypes', ncol = 1)
+
+ggsave(filename = paste0(resDir, '/Whiteddataset_tSNE_dpa23_VlnPlot.pdf'), 
+       width = 8, height = 28)
+
+write.csv2(features, file = paste0(resDir, '/Whiteddataset_dpa23_featuresList.csv'), 
+           row.names = FALSE)
 
 
